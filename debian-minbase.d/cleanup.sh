2ffaa82720a86b0ad639a0c7db288a1a4f43b5c4
Konstantin Demin <rockdrilla@gmail.com>
2021-04-15 02:09:49 +0300

    debian-minbase: cleanup: fix locale setup

--- debian-minbase.d/cleanup.sh
+++ debian-minbase.d/cleanup.sh
@@ -5,7 +5,7 @@
 ## reset locale to default one
 unset LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES
 unset LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT LC_IDENTIFICATION
-export LANG=C.UTF8
+export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 
 ## setup various environment variables representing temporary directory
