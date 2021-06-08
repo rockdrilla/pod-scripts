8effdfe0772bc445d6fa9d9ba8d57eabbbb064e2
Konstantin Demin <rockdrilla@gmail.com>
2021-06-08 18:54:30 +0300

    minbase: introduce clean-glob-x.sh
    /opt/clean-glob-x.sh is more generic and reusable version of /opt/cleanup.d/dpkg-path-filter.
    it offers more accurate glob matching because it converts each glob to regex and then match path via find(1).
    
    also:
    - reuse /opt/clean-glob-x.sh in /opt/cleanup.d/dpkg-path-filter

---
+++ minbase/opt/clean-glob-x.sh
@@ -0,0 +1,198 @@
+#!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# (c) 2021, Konstantin Demin
+set -f
+
+######################################################################
+## script internals
+
+## special magic with separator for sed "s" command
+## char 027 (0x17) seems to be safe separator for sed "s" command;
+## idea taken from Debian src:nginx/debian/dh_nginx
+X=$(command printf '\027')
+
+## $1 - match
+## $2 - replacement
+## $3 - flags (optional)
+repl() { command printf "s${X}%s${X}%s${X}%s" "$1" "$2" "$3" ; }
+
+esc_dots=$( repl '\.' '\\.' g )
+esc_qmarks=$( repl '([^\]|^)\?' '\1.' g )
+
+esc_head=$( repl '^\*{2}/' '.+/' g )
+esc_tail=$( repl '/\*{2}$' '(/.+)?' g )
+esc_mid=$( repl '(/|^)\*{2}(/|$)' '\1(.+\2)?' g )
+esc_any=$( repl '([^*]|^)\*{2}([^*]|$)' '\1.+\2' g )
+esc_double_star="${esc_head};${esc_tail};${esc_mid};${esc_any}"
+
+esc_mid=$( repl '(/|^)\*(/|$)' '\1[^/]+\2' g )
+esc_any=$( repl '([^*]|^)\*([^*]|$)' '\1[^/]*\2' g )
+esc_single_star="${esc_mid};${esc_any}"
+
+esc_stars="${esc_double_star};${esc_single_star}"
+
+trim_dup_slashes=$(repl '//+' '/' g)
+
+## TODO: discover more cases
+
+esc_all="${esc_dots};${esc_qmarks};${esc_stars};${trim_dup_slashes}"
+
+rx_glob_esc() { printf '%s' "$1" | sed -E "${esc_all};" ; }
+
+add_anchors=$( repl '^(.+)$' '^\1$' g )
+
+## set sail... or not :D
+rx_glob_moor() { printf '%s' "$1" | sed -E "${add_anchors};" ; }
+
+rx_glob() { printf '%s' "$1" | sed -E "${esc_all};${add_anchors};" ; }
+
+test_regex() { sed -En "\\#$1#p" </dev/null ; }
+
+######################################################################
+## script itself
+
+sysroot_skiplist='^/(sys|proc|dev)$'
+cfg_stanza='^(ex|in)clude=(.+)$'
+
+## recursion: turn glob to regex and then into file list 
+## (symlinks are listed too!)
+case "$1" in
+--ex|--in)
+	set +f
+
+	## $1 - action (ex[clude] / in[clude])
+	## $2 - path glob (one argument!)
+
+	action="${1#--}"
+	path_glob="$2"
+
+	x=$(printf '%s' "$2" | cut -c 1)
+	if [ "$x" != '/' ] ; then
+		path_glob="${TOPMOST_D}/${path_glob}"
+	fi
+
+	path_regex=$(rx_glob "${path_glob}")
+	if ! test_regex "${path_regex}" ; then
+		cat 1>&2 <<-EOF
+		Bad regex was produced from glob:
+		  directory: ${TOPMOST_D}
+		  glob: $2
+		  regex: ${path_regex}
+
+		Please report this case to developers.
+		EOF
+		exit 1
+	fi
+
+	result=$(mktemp -p "${RESULT_D}" "${action}.XXXXXXXX")
+
+	if [ "$x" = '/' ] ; then
+		## absolute glob is searched FS-wide
+		find / -mindepth 1 -maxdepth 1 -type d -print0 \
+		| grep -zEv "${sysroot_skiplist}" \
+		| xargs -0 -r -I'^^' \
+		  find '^^' -mindepth 1 -regextype egrep \
+		  -regex "${path_regex}" '!' -type d
+	else
+		## relative glob is searched under topmost directory
+		find "${TOPMOST_D}" -mindepth 1 -regextype egrep \
+		-regex "${path_regex}" '!' -type d
+	fi > "${result}"
+
+	exit 0
+;;
+esac
+
+# topmost="$1"
+conf="/dev/stdin"
+case "$#" in
+1) ;;
+2) [ "$2" != '-' ] && conf="$2" ;;
+*) exit 1 ;;
+esac
+
+if ! [ -r "${conf}" ] ; then
+	exit 1
+fi
+
+## reformat rules them like "--(ex|in)\0selector\0"
+trun=$(mktemp)
+grep -E "${cfg_stanza}" \
+< "${conf}" \
+| sort -uV \
+| sed -En "/${cfg_stanza}/{s//--\\1\\n\\2/;p;}" \
+| tr '\n' '\0' \
+> "${trun}"
+
+## nothing to filter at all
+if ! [ -s "${trun}" ] ; then
+	rm -f "${trun}"
+	exit 0
+fi
+
+## run ourself recursively and store results in directory
+if [ -z "${NPROC}" ] ; then
+	NPROC=$(nproc)
+	NPROC=$(( NPROC + (NPROC+1)/2 ))
+fi
+
+tparts=$(mktemp -d)
+
+env TOPMOST_D="$1" RESULT_D="${tparts}" \
+xargs -0 -n 2 -P "${NPROC}" "$0" \
+< "${trun}"
+
+rm -f "${trun}" ; unset trun
+
+## merge results to "save list"
+tINclude=$(mktemp)
+find "${tparts}/" -mindepth 1 -name 'in.*' -exec cat '{}' '+' \
+| sort -uV > "${tINclude}"
+
+## merge results to "remove list"
+tEXclude=$(mktemp)
+find "${tparts}/" -mindepth 1 -name 'ex.*' -exec cat '{}' '+' \
+| sort -uV > "${tEXclude}"
+
+rm -rf "${tparts}" ; unset tparts
+
+## nothing to filter at all (again)
+if ! [ -s "${tEXclude}" ] ; then
+	rm -f "${tINclude}" "${tEXclude}"
+	exit 0
+fi
+
+## filter out files in "save list"
+tremove=$(mktemp)
+if [ -s "${tINclude}" ]
+then grep -Fxv -f "${tINclude}"
+else cat
+fi < "${tEXclude}" \
+| tr '\n' '\0' \
+> "${tremove}"
+
+rm -f "${tINclude}" "${tEXclude}" ; unset tINclude tEXclude
+
+## nothing to filter at all (again?)
+if ! [ -s "${tremove}" ] ; then
+	rm -f "${tremove}"
+	exit 0
+fi
+
+## remove files already!
+## ... or not if env DRY is not empty :)
+if [ -n "${DRY}" ] ; then
+	cat <<-EOF
+	## ENV DRY=${DRY} was specified, no files are removed!
+	matched files:
+	EOF
+	tr '\0' '\n'
+else
+	if [ -n "${PIPELINE}" ] ; then
+		cat
+	else
+		xargs -0 -r rm -f
+	fi
+fi < "${tremove}"
+
+rm -f "${tremove}" ; unset tremove
