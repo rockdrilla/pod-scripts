94885f7e8c13da28b5ff870a0299d2b4ded5517e
Konstantin Demin <rockdrilla@gmail.com>
2021-06-10 09:18:21 +0300

    treewide: utilize xglob.sh where applicable
    NB: use "set -f" (noglob) in conjunction with xglob.sh to avoid errors.

--- golang/build.sh
+++ golang/build.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # (c) 2021, Konstantin Demin
 
-set -e
+set -e -f
 
 tar -C "$1" -xf "$2"
 cd "$1/go/src/"
@@ -10,11 +10,11 @@ cd "$1/go/src/"
 [ -x "$1/go/bin/go" ]
 export PATH="$1/go/bin:${PATH}"
 go install std
-cd "$1/go/"
-rm -rf \
-    pkg/*/cmd \
-    pkg/bootstrap \
-    pkg/obj \
-    pkg/tool/*/api \
-    pkg/tool/*/go_bootstrap \
-    src/cmd/dist/dist
+XGLOB_DIRS=1 /opt/xglob.sh "$1/go/" <<-EOF
+	delete=pkg/**/cmd
+	delete=pkg/bootstrap
+	delete=pkg/obj
+	delete=pkg/tool/**/api
+	delete=pkg/tool/**/go_bootstrap
+	delete=src/cmd/dist/dist
+EOF
